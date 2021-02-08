49cec37ed47006c422151499784438ec72b627c1
Undead9000 <undead10000@gmail.com>
2021-02-08 18:30:23 +0200

    Added slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
 // Main js file
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
+$(".workflow__slider").data( "js-handler", "slider" ).slick({
+    dots: true,
+    appendDots: $('.workflow'),
+    appendArrows: $('.workflow__container')
+});
+
+$(".recommendations__slider").data( "js-handler", "slider" ).slick({
+  dots: false,
+  appendArrows: $('.recommendations__slider')
+});
