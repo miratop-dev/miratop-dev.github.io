2c63859ad943a059236d52451289380de729d076
Undead9000 <undead10000@gmail.com>
2021-02-18 14:19:47 +0200

    Prettifying files

--- src/js/index.js
+++ src/js/index.js
@@ -57,20 +57,30 @@ $("[data-js-handler='filter-block-more__handler']").on('click', function(e){
   })
 })
 
-$( function() {
-  $("[data-js-handler='range-slider__handler']").slider({
-    range: true,
-    min: 0,
-    max: 10,
-    step: 0.1,
-    values: [4.5, 9.5],
-    slide: function( event, ui ) {
-      $('.range-slider__min').val(ui.values[ 0 ]);
-      $('.range-slider__max').val(ui.values[ 1 ]);
-    }
-  });
-  $('.range-slider__min').val(
-    $("[data-js-handler='range-slider__handler']").slider( "values", 0 )
-  );
-  $('.range-slider__max').val($("[data-js-handler='range-slider__handler']").slider( "values", 1 ) );
-} );
+$("[data-js-handler='range-slider__handler']").slider({
+  range: true,
+  min: 0,
+  max: 10,
+  step: 0.1,
+  values: [4.5, 9.5],
+  slide: function( event, ui ) {
+    $('.range-slider__min').val(ui.values[ 0 ]);
+    $('.range-slider__max').val(ui.values[ 1 ]);
+  }
+});
+$('.range-slider__min').val(
+  $("[data-js-handler='range-slider__handler']").slider( "values", 0 )
+);
+$('.range-slider__min').on('change', function(e){
+  $("[data-js-handler='range-slider__handler']").slider(
+    'values', 0, $(this).val()
+  )
+});
+$('.range-slider__max').val(
+  $("[data-js-handler='range-slider__handler']").slider( "values", 1 ) 
+);
+$('.range-slider__max').on('change', function(e){
+  $("[data-js-handler='range-slider__handler']").slider(
+    'values', 1, $(this).val()
+  )
+})
