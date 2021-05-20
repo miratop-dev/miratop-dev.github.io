7a644af85bb713f3319fa15796c983c49277e740
Undead9000 <undead10000@gmail.com>
2021-05-20 12:42:01 +0300

    Quick fix for password validation's messages

--- src/js/index.js
+++ src/js/index.js
@@ -291,13 +291,13 @@ $("[data-js-handler='password-validation']").on('input', function(e){
   if(pass !== $(this).val()) {
     $(this).addClass('error');
     $('#password').addClass('error');
-    $("[data-js-handler='error-message']").removeClass('hide').children('p').text('Паролі не співпадають');
+    $("[data-js-handler='error-message']").removeClass('hide').find('p').text('Паролі не співпадають');
     $("[data-js-handler='success-message']").addClass('hide')
   } else {
     $(this).removeClass('error');
     $('#password').removeClass('error');
     if($(this).length > 0) {
-      $("[data-js-handler='success-message']").removeClass('hide').children('p').text('Паролі співпадають');
+      $("[data-js-handler='success-message']").removeClass('hide').find('p').text('Паролі співпадають');
       $("[data-js-handler='error-message']").addClass('hide')    
     }
   }
